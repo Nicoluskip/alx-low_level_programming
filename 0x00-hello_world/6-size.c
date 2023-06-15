68ad586d8235efc6f55d0118c1cbc44502b61161
Nicoluslip <tnic276@gmail.com>
2023-06-15 15:15:59 -0700

    Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,4 @@
 #include <stdio.h>
-/**
- * main - main block
- * Return: 0
- */
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
@@ -11,3 +7,4 @@ int main(void)
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
 	return (0);
+}
